df391c7f89ef60611fef2c78173d9dccacf0fad5
Felipe <feferodrigues771@gmail.com>
2023-06-04 17:29:13 -0300

    Profile models

--- fetch_pets/core/models.py
+++ fetch_pets/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    nome_usuario = models.CharField(max_length=100, verbose_name='Nome')
+    sobrenome_usuario = models.CharField(max_length=100, verbose_name='Sobrenome')
+    email = models.EmailField(max_length=100, verbose_name='E-mail')
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+
+    def __str__(self):
+        return self.user.username
--- fetch_pets/fetch_pets/settings.py
+++ fetch_pets/fetch_pets/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -118,8 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
